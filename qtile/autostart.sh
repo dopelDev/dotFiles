a020bd0d0aa075fe778940c9b0febde948f6af34
dopeldev <322kuroneko@gmail.com>
2021-03-19 16:21:25 -0400

    add qtile config

---
+++ qtile/autostart.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+#xrandr --auto --output HDMI-0 --primary
+xrandr --auto --output HDMI-0 --primary --right-of DP-0 --auto --output DVI-I-0 --mode 1368x768 --right-of HDMI-0 --auto --output DP-0 
